6f56bd8fd2a658e37f2f0b88aa9c710d0982c39e
dee_mavr <mavridou.d.p@gmail.com>
2022-01-01 20:48:25 +0000

    added graph1

--- flaskapp/application/routes.py
+++ flaskapp/application/routes.py
@@ -3,7 +3,37 @@ from flask import render_template, url_for
 import pandas as pd
 import plotly
 import plotly.express as px
+import json
 
 @app.route("/")
 def index():
-    return render_template("layout.html", title="Home")
+
+    # Graph One
+    df = pd.read_csv('../data/avg_yearly.csv')
+
+    fig1 = px.line(df, x='year_', y=['women_perc','RM5'],
+            title='Women Representation in Parliament (Global Average) from 1945 to 2018',
+            markers=True,
+            labels=dict(year_='Year', women_perc='Participation (%)')
+            )
+    fig1.update_yaxes(title_text='Women (%)')
+
+    fig1.update_layout(
+            font_family="Courier New",
+            font_color="grey",
+            title_font_family="Courier New",
+            title_font_color="#1f77b4",
+            legend_title_font_color="grey",
+            autosize=False,
+            width=800,
+            height=500
+        )
+    fig1.update_xaxes(nticks=10)
+    fig1.update_xaxes(rangeslider_visible=True)
+
+    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+    
+
+
+    return render_template("index.html", title="Home", graph1JSON = graph1JSON)
