978ed96d7228754f0e5a43352897069d64dd8723
dee_mavr <mavridou.d.p@gmail.com>
2022-01-01 23:20:32 +0000

    added fig4

--- flaskapp/application/routes.py
+++ flaskapp/application/routes.py
@@ -4,12 +4,15 @@ import pandas as pd
 import plotly
 import plotly.express as px
 import json
+import plotly.graph_objects as go
+
 
 @app.route("/")
 def index():
 
     # Graph One
     df = pd.read_csv('../data/avg_yearly.csv')
+    df['women_perc'] = round(df['women_perc'], 2)
 
     fig1 = px.line(df, x='year_', y=['women_perc','RM5'],
             title='Women Representation in Parliament (Global Average) from 1945 to 2018',
@@ -49,11 +52,40 @@ def index():
             width=800,
             height=400
             )
-
+    
     graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
 
+    # Graph 3
+    fig3 = px.line(df, x="year", y="women_perc", color='decade', markers=True,
+              title='Women Representation in Parliament (Global Average) by Decade',
+              labels=dict(year='Year', women_perc='Women (%)', decade='Decade')
+    )
+    fig3.update_layout(
+            font_family="Courier New",
+            font_color="grey",
+            title_font_family="Courier New",
+            title_font_color="#1f77b4",
+            legend_title_font_color="grey",
+            autosize=False,
+            width=800,
+            height=400
+            )
+    fig3.update_xaxes(nticks=20)
 
+    graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Graph 4
+    df_dec_avg = pd.DataFrame(df.groupby(['decade'])['women_perc'].median())
+    df_dec_avg = df_dec_avg.sort_values('women_perc')
+    df_dec_avg['perc_change'] = round( df_dec_avg['women_perc'].pct_change(), 2)
+
+    fig4 = go.Figure(data=[go.Table(header=dict(values=['Decade', 'Percentage Increase']),
+                    cells=dict(values=[df_dec_avg.index[1:], df_dec_avg['perc_change'].iloc[1:]]))
+                        ])
+    fig4.update_layout(autosize=False,  width=350, height=350)
+
+    graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
     
 
 
-    return render_template("index.html", title="Home", graph1JSON = graph1JSON, graph2JSON = graph2JSON)
+    return render_template("index.html", title="Home", graph1JSON = graph1JSON, graph2JSON = graph2JSON, graph3JSON = graph3JSON, graph4JSON = graph4JSON)
